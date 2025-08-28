d6b704cf6b49800b796bdb5abaaefef1e51303aa
Martin Lablans <code@martin.lablans.de>
2025-08-28 13:06:38 +0200

    Client-side monitoring

--- setup/steps/1-autoupdates.sh
+++ setup/steps/1-autoupdates.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-systemctl enable rpm-ostreed-automatic.timer
+systemctl enable rpm-ostreed-automatic.timer report-updates.path
 
 sed -i 's/^AutomaticUpdatePolicy=.*/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
