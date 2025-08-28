a5328ee989ffce2736e9177dd717ea986276cc1f
Martin Lablans <code@martin.lablans.de>
2025-08-28 15:47:54 +0200

    Updated

--- setup/steps/1-autoupdates.sh
+++ setup/steps/1-autoupdates.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-systemctl enable rpm-ostreed-automatic.timer report-updates.path
+systemctl enable rpm-ostreed-automatic.timer report-updates.timer
 
 sed -i 's/^AutomaticUpdatePolicy=.*/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
